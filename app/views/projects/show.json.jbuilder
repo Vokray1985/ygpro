c915b86273211d45a46443a2ff4ff4bebf4be53f
vokray <vokraye@gmail.com>
2016-07-13 11:07:22 +0000

    init repo

---
+++ app/views/projects/show.json.jbuilder
@@ -0,0 +1,1 @@
+json.extract! @project, :id, :title, :description, :file_url, :created_at, :updated_at
---
+++ config/initializers/assets.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+Rails.application.config.assets.precompile += %w( projects.css )
+Rails.application.config.assets.precompile += %w( projects.js )
